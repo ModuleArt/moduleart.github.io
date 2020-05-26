ceb4a6077379f3e582a0ae03dee56d4d5412cba6
Eugene Volynko <volynko.ua@gmail.com>
2020-05-26 11:05:50 +0300

    website2.0

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    GetLatestReleaseInfo();  
+});  
+
+function GetLatestReleaseInfo() {
+    let repositories = [ "quick-picture-viewer", "quick-screen-recorder", "quick-color-picker" ];
+    let filenames = [ "QuickPictureViewer", "QuickScreenRecorder", "QuickColorPicker" ];
+
+    $.each(repositories, function(key, value) {
+        $.getJSON("https://api.github.com/repos/ModuleArt/" + value + "/tags").done(function (json) {
+            let link = document.getElementById(value + "-link");
+            link.href = "https://github.com/ModuleArt/" + value + "/releases/download/" + json[0].name + "/" + filenames[key] + "-Setup.msi";
+            link.getElementsByClassName("version")[0].innerHTML = json[0].name;
+        });
+    });
+} 
