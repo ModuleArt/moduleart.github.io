ead86370bcd1703ccaf89534a62231445e45ee39
Eugene Volynko <volynko.ua@gmail.com>
2020-09-09 21:35:33 +0300

    Added latest release notes blocks

--- js/index.js
+++ js/index.js
@@ -25,25 +25,37 @@ function GetReposInfo() {
     let repositories = [ "quick-picture-viewer", "quick-screen-recorder", "quick-color-picker" ];
     let filenames = [ "QuickPictureViewer", "QuickScreenRecorder", "QuickColorPicker" ];
 
-    // download link
+    let converter = new showdown.Converter();
+
     $.each(repositories, function(key, value) {
-        $.getJSON("https://api.github.com/repos/ModuleArt/" + value + "/tags").done(function (json) {
+        $.getJSON("https://api.github.com/repos/ModuleArt/" + value + "/releases/latest").done(function (json) {
+            // latest release info
+            let relInfo = document.getElementById(value).getElementsByClassName("release-info")[0];
+            relInfo.innerHTML = converter.makeHtml("#" + json.name + "\n" + json.body);
+            let relBtn = document.getElementById(value).getElementsByClassName("release-btn")[0];
+            relBtn.innerHTML = json.tag_name + " - Release notes";
+
+            // download link
             let link = document.getElementById(value).getElementsByClassName("download-windows")[0];
-            link.href = "https://github.com/ModuleArt/" + value + "/releases/download/" + json[0].name + "/" + filenames[key] + "-Setup";
+            link.href = "https://github.com/ModuleArt/" + value + "/releases/download/" + json.tag_name + "/" + filenames[key] + "-Setup";
             if (key == 0) {
                 link.href += ".exe";
             } else {
                 link.href += ".msi";
             }
-            link.getElementsByClassName("version")[0].innerHTML = json[0].name;
+            link.getElementsByClassName("version")[0].innerHTML = json.tag_name;
         });
-    });
 
-    // repo description
-    $.each(repositories, function(key, value) {
+        // repo description
         $.getJSON("https://api.github.com/repos/ModuleArt/" + value).done(function (json) {
             let desc = document.getElementById(value).getElementsByClassName("desc")[0];
             desc.innerHTML = json.description;
         });
     });
 } 
+
+function toggleReleaseInfo(btn) {
+    btn.classList.toggle("show");
+    let relInfo = btn.parentNode.getElementsByClassName("release-info")[0];
+    relInfo.classList.toggle("show");
+}
