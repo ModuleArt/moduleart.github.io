fd32c142b8cfe76ca6256feca55584fc89d18ab5
Eugene Volynko <volynko.ua@gmail.com>
2020-12-16 23:59:33 +0200

    Brand new website styling

--- js/index.js
+++ js/index.js
@@ -1,79 +1,38 @@
 $(document).ready(function () {
-    updateMasonry();
-
-    $(window).resize(function() {
-        updateMasonry();
-    });
-
-    $(".screenshot").magnificPopup({
-        type: "image",
-        removalDelay: 300,
-        mainClass: "mfp-fade",
-        gallery: {
-            enabled: true,
-            tCounter: ""
-          },
-        image: {
-            titleSrc: function(item) {
-                let icon = item.el[0].parentNode.getElementsByClassName("icon")[0];
-                let title = item.el[0].parentNode.getElementsByClassName("title")[0];
-                let links = item.el[0].parentNode.getElementsByClassName("links")[0];
-
-                return icon.outerHTML + title.outerHTML + links.outerHTML;
-            }
-        }
-    });
-
-    GetReposInfo();
-});  
-
-function GetReposInfo() {
-    let repositories = [ "quick-picture-viewer", "quick-screen-recorder", "quick-color-picker" ];
-    let filenames = [ "QuickPictureViewer", "QuickScreenRecorder", "QuickColorPicker" ];
-
     let converter = new showdown.Converter();
 
-    $.each(repositories, function(key, value) {
-        $.getJSON("https://api.github.com/repos/ModuleArt/" + value + "/releases/latest").done(function (json) {
-            // latest release info
-            let relInfo = document.getElementById(value).getElementsByClassName("release-info")[0];
-            relInfo.innerHTML = converter.makeHtml("#" + json.name + "\n" + json.body);
-            let relBtn = document.getElementById(value).getElementsByClassName("release-btn")[0];
-            relBtn.innerHTML = json.tag_name + " - Release notes";
+    $.getJSON("https://api.github.com/repos/ModuleArt/quick-picture-viewer/releases/latest").done(function(json) {
 
-            // download link
-            let link = document.getElementById(value).getElementsByClassName("download-windows")[0];
-            link.href = "https://github.com/ModuleArt/" + value + "/releases/download/" + json.tag_name + "/" + filenames[key] + "-Setup";
-            if (key == 0) {
-                link.href += ".exe";
-            } else {
-                link.href += ".msi";
-            }
-            link.getElementsByClassName("version")[0].innerHTML = json.tag_name;
-        });
+        // download link
+        let downloadLink = document.getElementById("download-link");
+        downloadLink.href = "https://github.com/ModuleArt/quick-picture-viewer/releases/download/" + json.tag_name + "/QuickPictureViewer-Setup.exe";
+        downloadLink.getElementsByClassName("version")[0].innerHTML = json.tag_name;
+        downloadLink.getElementsByClassName("size")[0].innerHTML = "Win 7-10, " + formatBytes(json.assets[0].size);
 
-        // repo description
-        $.getJSON("https://api.github.com/repos/ModuleArt/" + value).done(function (json) {
-            let desc = document.getElementById(value).getElementsByClassName("desc")[0];
-            desc.innerHTML = json.description;
-            updateMasonry(desc.parentNode.parentNode);
+        // what's new
+        let whatsNewLink = document.getElementById("whats-new").getElementsByClassName("release-title")[0];
+        whatsNewLink.innerHTML = json.name;
+        whatsNewLink.href = "https://github.com/ModuleArt/quick-picture-viewer/releases/tag/" + json.tag_name;
+        document.getElementById("whats-new").getElementsByClassName("release-date")[0].innerHTML = " - " + new Date(json.published_at).toLocaleDateString('en-US', {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
         });
+        document.getElementById("whats-new").getElementsByClassName("release-markdown")[0].innerHTML = converter.makeHtml(json.body);
+
+        // copyright year
+        document.getElementById("copyright").innerHTML = "Copyright © " + new Date().getFullYear() + " Module Art";
     });
-} 
+});
 
-function toggleReleaseInfo(btn) {
-    btn.classList.toggle("show");
-    let relInfo = btn.parentNode.getElementsByClassName("release-info")[0];
-    relInfo.classList.toggle("show");
-    updateMasonry(btn.parentNode.parentNode);
-}
+function formatBytes(bytes, decimals = 1) {
+    if (bytes === 0) return '0 Bytes';
 
-function updateMasonry(section) {
-    if (section != null) {
-        $(section).masonry();
-    } else {
-        $(".tile-section").each(function(index, element) {
-            $(element).masonry();
-        });
-    }
+    const k = 1024;
+    const dm = decimals < 0 ? 0 : decimals;
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
 }
